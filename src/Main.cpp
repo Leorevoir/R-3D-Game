89574a8a4256f44fa4116d69dd748d3d7fb4462a
Leorevoir <leo.quinzler@epitech.eu>
2025-10-27 20:42:55 +0100

    add: minimal main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,19 @@
-#include "R-Engine/Application.hpp"
-#include "R-Engine/Plugins/DefaultPlugins.hpp"
-#include "R-Engine/Plugins/WindowPlugin.hpp"
+#include <R-Engine/Application.hpp>
 
-#include "R-Game/Startup.hpp"
-#include "R-Game/Update.hpp"
+#include <R-Engine/Plugins/DefaultPlugins.hpp>
+#include <R-Engine/Plugins/WindowPlugin.hpp>
 
+/**
+ * private
+ */
 // clang-format off
+
+static const r::WindowPluginConfig g_window_config
+{
+    .size = {1920, 1080},
+    .title = "R-Engine - R-Game 3D",
+    .cursor = r::WindowCursorState::Locked
+};
 
 /**
 * public
@@ -15,25 +23,10 @@ i32 main(void)
 {
     r::Application{}
 
-        .add_plugins(r::DefaultPlugins{}.set(
-            r::WindowPlugin{
-                r::WindowPluginConfig
-                {
-                    .size = {1920, 1080},
-                    .title = "R-Engine - R-Game 3D",
-                    .cursor = r::WindowCursorState::Locked
-                }
-            }
-        ))
+        .add_plugins(r::DefaultPlugins{}.set(g_window_config))
 
         .add_systems<
-            r::Startup::create_player,
-            r::Startup::create_inputs
         >(r::Schedule::STARTUP)
-
-        .add_systems<
-            r::Update::camera
-        >(r::Schedule::FIXED_UPDATE)
 
         .run();
 }
