792de4e176e48d2b8594e009024b16684f84d51f
Leorevoir <leo.quinzler@epitech.eu>
2025-10-28 23:04:22 +0100

    add: preprocessor ifdefined game debug then r engine debug is too

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,21 +1,12 @@
+#if defined(R_GAME_DEBUG) && !defined(ENGINE_DEBUG)
+    #define ENGINE_DEBUG
+#endif
+
 #include <R-Engine/Application.hpp>
 #include <R-Engine/Plugins/InputPlugin.hpp>
 
 #include <Gwent/Plugins/Default.hpp>
 #include <Gwent/States.hpp>
-
-#include <iostream>
-
-/**
- * debug
- */
-
-static void debug_system(r::ecs::Res<r::UserInput> input_ptr)
-{
-    const auto &input = input_ptr.ptr;
-
-    std::cout << "\rmouse={" << input->mouse_position.x << ", " << input->mouse_position.y << "}" << std::flush;
-}
 
 /**
 * public
@@ -27,6 +18,5 @@ i32 main(void)
 
         .init_state(r::gwent::State::ChoosingDeck)
         .add_plugins(r::gwent::Default{})
-        .add_systems<debug_system>(r::Schedule::UPDATE)
         .run();
 }
