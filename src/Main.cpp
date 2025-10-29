a46ef14bab4bd0ab26d0e3dcb72547c2740cd0e9
Leorevoir <leo.quinzler@epitech.eu>
2025-10-29 19:26:10 +0100

    add: __debug_system

--- src/Main.cpp
+++ src/Main.cpp
@@ -8,6 +8,23 @@
 #include <Gwent/Plugins/Default.hpp>
 #include <Gwent/States.hpp>
 
+#include <iostream>
+
+namespace {
+
+/**
+ * debug system
+ */
+
+#if defined(R_GAME_DEBUG)
+static void __debug_system(r::ecs::Res<r::UserInput> input) noexcept
+{
+    std::cout << "\rpos={" << input.ptr->mouse_position.x << "," << input.ptr->mouse_position.y << "}" << std::flush;
+}
+#endif
+
+}// namespace
+
 /**
 * public
 */
@@ -18,5 +35,8 @@ i32 main(void)
 
         .init_state(r::gwent::State::ChoosingDeck)
         .add_plugins(r::gwent::Default{})
+#if defined(R_GAME_DEBUG)
+        .add_systems<__debug_system>(r::Schedule::UPDATE)
+#endif
         .run();
 }
