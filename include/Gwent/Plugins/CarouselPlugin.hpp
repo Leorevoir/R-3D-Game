a9a837649d19d5a1664dc528807bbfaf4a89dc21
Leorevoir <leo.quinzler@epitech.eu>
2025-10-30 13:36:08 +0100

    feat: make exportable the carousel state

--- include/Gwent/Plugins/CarouselPlugin.hpp
+++ include/Gwent/Plugins/CarouselPlugin.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <R-Engine/ECS/Entity.hpp>
 #include <R-Engine/Plugins/Plugin.hpp>
 
 namespace r {
 
 namespace gwent {
+
+struct CarouselState final {
+        std::vector<ecs::Entity> hand;
+        std::vector<ecs::Entity> deck;
+        u8 swaps_left = 2;
+};
 
 class CarouselPlugin final : public Plugin
 {
